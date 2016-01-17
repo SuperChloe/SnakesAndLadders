36844e428342c59e9f9b381d43f087ac50be1a18
Chloe Horgan <super.chloe@gmail.com>
2016-01-17 15:11:24 -0500

    Initial Commit

---
+++ Snakes and Ladders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Snakes and Ladders
+//
+//  Created by Chloe on 2016-01-17.
+//  Copyright © 2016 Chloe Horgan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

ea9ac0f3afcd8f4b0c4093f5861a0d69dcaee1ef
Chloe Horgan <super.chloe@gmail.com>
2016-01-17 17:31:01 -0500

    Make GameController

---
+++ Snakes and Ladders/GameController.h
@@ -0,0 +1,13 @@
+//
+//  GameController.h
+//  Snakes and Ladders
+//
+//  Created by Chloe on 2016-01-17.
+//  Copyright © 2016 Chloe Horgan. All rights reserved.
+//
+
+#import "SpaceContent.h"
+
+@interface GameController : SpaceContent
+
+@end
---
+++ Snakes and Ladders/GameController.m
@@ -0,0 +1,13 @@
+//
+//  GameController.m
+//  Snakes and Ladders
+//
+//  Created by Chloe on 2016-01-17.
+//  Copyright © 2016 Chloe Horgan. All rights reserved.
+//
+
+#import "GameController.h"
+
+@implementation GameController
+
+@end
